dcfb69ca469d30e3bf1fa25cad37dbff65a0f1a9
takayuki.kayawari <kayamelo@gmail.com>
2017-08-25 23:44:51 +0900

    ログイン後、セッションにuser_idを保存

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # TODO: マスターにマージしたらスペック書く
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
       redirect_to user_url(@user)
     else
       flash.now[:danger] = 'ログインできません'
