5ffac298335b179e14f1d09abcbdda9c006e537a
takayuki.kayawari <kayamelo@gmail.com>
2017-08-28 23:36:36 +0900

    日記登録画面作成

---
+++ app/controllers/diaries_controller.rb
@@ -0,0 +1,20 @@
+class DiariesController < ApplicationController
+  def index
+  end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
