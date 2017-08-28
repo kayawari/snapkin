18507dbc9f2554e6bb5f6666860394ba91ff20cf
takayuki.kayawari <kayamelo@gmail.com>
2017-08-28 23:56:30 +0900

    referencesの名前がおかしいので修正

--- db/migrate/20170731131726_create_diaries.rb
+++ db/migrate/20170731131726_create_diaries.rb
@@ -3,10 +3,10 @@ class CreateDiaries < ActiveRecord::Migration[5.1]
     create_table :diaries do |t|
       t.string      :title,    null: false
       t.text        :content,  null: false
-      t.references  :image_id, null: false
+      t.references  :image, null: false
       t.string      :lat,      null: false, comment: '緯度'
       t.string      :lng,      null: false, comment: '経度'
-      t.references  :category_id
+      t.references  :category
       t.datetime    :journey_time
       t.timestamps
     end
